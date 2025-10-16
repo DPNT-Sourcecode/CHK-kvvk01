b353210cb0136aff940bb45880771083020a1089
sbx_user1051 <sbx_user1051@169.254.58.1>
2025-10-16 20:44:37 +0000

    Thu Oct 16 20:44:37 UTC 2025

--- lib/tests/test_sum_solution.py
+++ lib/tests/test_sum_solution.py
@@ -9,7 +9,9 @@ def calculator():
 
 @pytest.mark.parametrize("x,y, expected", [
     (0, 1, 1),
+    (2, 6, 8),
+    (1, 100, 101),
 ])
-
+    
 def test_sum_with_valid_inputs(calculator, x, y, expected):
     assert calculator.sum(x, y) == expected
