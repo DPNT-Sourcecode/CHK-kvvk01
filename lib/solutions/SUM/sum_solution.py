026b0566b732a78329f159e3aa00cfb5225230f9
sbx_user1051 <sbx_user1051@169.254.58.1>
2025-10-16 20:20:37 +0000

    Thu Oct 16 20:20:37 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,18 @@
+from operator import truediv
+
 
 class SumSolution:
     
     def compute(self, x, y):
         raise NotImplementedError()
+
+    def _validate_input_sum(self, x:int) -> bool:
+        if isinstance(x, int):
+            return true
+
+    def sum(self, x: int, y: int) -> int:
+        """Calculate sum between input numbers in range 1-100"""
+        ## validate input range and type
+            # raise error
+
+        return x + y
