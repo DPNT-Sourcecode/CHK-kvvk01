3161285e164e672847a92c359d44a2661974cde1
sbx_user1051 <sbx_user1051@169.254.58.1>
2025-10-16 20:53:37 +0000

    Thu Oct 16 20:53:37 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,11 +1,11 @@
-from operator import truediv
 
 class SumSolution:
     
     def compute(self, x, y):
         raise NotImplementedError()
 
-    def _isValidInput(self, x:int) -> bool:
+    @staticmethod
+    def _is_valid_input(x:int) -> bool:
         if not isinstance(x, int):
             raise TypeError("value must be a positive integer")
         if not  0 <= x <=100:
@@ -15,7 +15,8 @@ class SumSolution:
 
     def sum(self, x: int, y: int) -> int:
         """Calculate sum between input numbers in range 1-100"""
-        if self._isValidInput(x) and self._isValidInput(y):
+        if self._is_valid_input(x) and self._is_valid_input(y):
             return x + y
         return 0
+
 
