0a32b26f8cde9438e6a818356863a066d7a3e373
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-21 23:44:21 +0000

    Tue Oct 21 23:44:21 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,13 @@
 
+
+
+# parse the string into an appropiate data structure
+# process each item i
+# apply business rules to item
+
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
         raise NotImplementedError()
+
