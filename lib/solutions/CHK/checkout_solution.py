e4a9523e64ca4481eb6fc0e219c8009102ed37ce
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-22 00:26:21 +0000

    Wed Oct 22 00:26:21 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,31 +7,34 @@ from dataclasses import dataclass
 
 # what does an example string look likE??
 
-
-
+@dataclass
+class Item:
+    price: int
+    quantity: int
+    offer: tuple | None
 
 class CheckoutSolution:
-    @dataclass
-    class Item:
-        price: int
-        quantity: int
-        offer: tuple | None
-
-    inventory = {"A":  Item(50, 0, (3, 130)), "B": Item(30, 0, (2, 45), "C": Item(20, 0, None), "D", Item(15, 0, None)}
 
     # skus = unicode string
     def checkout(self, skus) -> int :
 
-
+        inventory = {"A": Item(50, 0, (3, 130)),
+                     "B": Item(30, 0, (2, 45)),
+                     "C": Item(20, 0, None),
+                     "D": Item(15, 0, None)}
          if not isinstance(skus, str):
              return -1
 
+         for ch in skus:
+             if ch not in inventory.keys()
+
+
          if skus == "":
              return 0
-
-         for ch in skus
+             
 
 
         total = 0
         return total
+
 
